95aa14d902bf679d0605c6be40adc864871e174d
Alan Drozd <alanezedrozd@gmail.com>
2022-07-01 03:20:22 -0300

    edit

--- js/index.js
+++ js/index.js
@@ -10,23 +10,23 @@ class Product {
   }
 }
 
-articles.push(new Product(01, "Polerin Yaro Negro", 4790, "Ropa", "./assets/images/card/card1.jpg"));
-articles.push(new Product(02, "Poleron Dean Crudo", 8990, "Ropa", "./assets/images/card/card2.jpg"));
-articles.push(new Product(03, "Jean Jacob", 8990, "Ropa", "./assets/images/card/card3.jpg"));
-articles.push(new Product(04, "Poleron Dean Negro", 8990, "Ropa", "./assets/images/card/card4.jpg"));
-articles.push(new Product(05, "Kit Male Grooming", 5890, "MakeUp", "./assets/images/card/makeup1.jpg"));
-articles.push(new Product(06, "Mascara en gel", 1520, "MakeUp", "./assets/images/card/makeup2.jpg"));
-articles.push(new Product(07, "Primer Filler", 1290, "MakeUp", "./assets/images/card/makeup3.jpg"));
-articles.push(new Product(08, "Corrector Liquido", 1290, "MakeUp", "./assets/images/card/makeup4.jpg"));
+articles.push(new Product("01", "Polerin Yaro Negro", 4790, "Ropa", "./assets/images/card/card1.jpg"));
+articles.push(new Product("02", "Poleron Dean Crudo", 8990, "Ropa", "./assets/images/card/card2.jpg"));
+articles.push(new Product("03", "Jean Jacob", 8990, "Ropa", "./assets/images/card/card3.jpg"));
+articles.push(new Product("04", "Poleron Dean Negro", 8990, "Ropa", "./assets/images/card/card4.jpg"));
+articles.push(new Product("05", "Kit Male Grooming", 5890, "MakeUp", "./assets/images/card/makeup1.jpg"));
+articles.push(new Product("06", "Mascara en gel", 1520, "MakeUp", "./assets/images/card/makeup2.jpg"));
+articles.push(new Product("07", "Primer Filler", 1290, "MakeUp", "./assets/images/card/makeup3.jpg"));
+articles.push(new Product("08", "Corrector Liquido", 1290, "MakeUp", "./assets/images/card/makeup4.jpg"));
 
 // console.log(articles)
-let carrito;
+let shoppingCart;
 
-if (JSON.parse(localStorage.getItem("carrito"))){
-    carrito = JSON.parse(localStorage.getItem(carrito))
+if (JSON.parse(localStorage.getItem("shoppingCart"))){
+    shoppingCart = JSON.parse(localStorage.getItem(shoppingCart))
 } else {
-    localStorage.setItem("carrito", JSON.stringify([]))
-    carrito = JSON.parse(localStorage.getItem("carrito"))
+    localStorage.setItem("shoppingCart", JSON.stringify([]))
+    shoppingCart = JSON.parse(localStorage.getItem("shoppingCart"))
 }
 
 function desplegarProducts() {
@@ -65,38 +65,27 @@ function agregarAlCarrito (e) {
     const btn = e.target;
     const id = btn.getAttribute('id') 
     const prodEncontrado = articles.find(art => art.id == id)
-    const enCarrito = carrito.find(art => art.id == prodEncontrado.id) 
+    const enCarrito = shoppingCart.find(art => art.id == prodEncontrado.id) 
     if (!enCarrito){
-        carrito.push({...prodEncontrado, cantidad: 1})
+        shoppingCart.push({...prodEncontrado, cantidad: 1})
     } else {
-        let carritoFiltrado = carrito.filter(art => art.id != enCarrito.id)
-        carrito = [...carritoFiltrado, {...enCarrito, cantidad: enCarrito.cantidad +1}]
+        let carritoFiltrado = shoppingCart.filter(art => art.id != enCarrito.id)
+        shoppingCart = [...carritoFiltrado, {...enCarrito, cantidad: enCarrito.cantidad +1}]
     }
-    console.log(carrito)
-    localStorage.setItem('carrito', JSON.stringify(carrito))
+    console.log(shoppingCart)
+    localStorage.setItem('shoppingCart', JSON.stringify(shoppingCart))
 }
-
-// const contador = document.getElementById('cartCounter')
-// contador.innerHTML = carrito.length
-
-// if(JSON.parse(localStorage.getItem('carrito'))) {
-//     carrito = JSON.parse(localStorage.getItem('carrito'))
-// } else {
-//     localStorage.setItem('carrito', JSON.stringify([]))
-//     carrito = JSON.parse(localStorage.getItem('carrito'))
-// }
-
 const totalCarrito = () => {
-    return carrito.reduce((acc, art) => acc + art.precio * art.cantidad, 0)
+    return shoppingCart.reduce((acc, art) => acc + art.price * art.cantidad, 0)
 }
 
-const body = document.getElementById('carrito')
-if(carrito.length == 0){
+const body = document.getElementById('shoppingCart')
+if(shoppingCart.length == 0){
     const texto = `
     <div class="cartContainer">
-        <h1 class='txtCarrito'>El carrito esta vacio</h1>
-        <a class='btnVolver' href='index.html'>
-            <button>VOLVER</button>
+        <h3 class='txtCarrito'>El carrito esta vacio</h1>
+        <a href='index.html'>
+            <button class='btnVolver'>VOLVER</button>
         </a>
     </div>`;
 body.innerHTML += texto;
@@ -128,13 +117,13 @@ body.innerHTML += texto;
             </tfoot>
         </table>
     </div>
-    <div class="btn-container">
-        <button class="btnTerminar">Terminar compra</button>
+    <div>
+        <button class="btnVolver">Terminar compra</button>
     </div>`
     body.innerHTML += table
     const tbody =document.getElementById('tbody')
-    for (let i = 0; i < carrito.length; i++) {
-        const element = carrito[i];
+    for (let i = 0; i < shoppingCart.length; i++) {
+        const element = shoppingCart[i];
         const { id, name, img, price, cantidad} = element;
         const cart = `
         <tr id=${id}>
